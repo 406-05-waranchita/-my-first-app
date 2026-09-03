4c2099664441cc36094db651de893bb5ef390d03
406-05-waranchita <54172@yupparaj.ac.th>
2026-09-03 11:23:09 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -53,14 +53,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
     # ตรวจข้อ 3
-    if u_ans3 == "Key":
+    if u_ans3 == "key":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans4 == "Mango":
+    if u_ans4 == "mango":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
